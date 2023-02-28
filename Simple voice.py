40b5047131a36af496e78b4b5154a8fb68645792
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-02-28 12:23:06 +0100

    Update Simple voice.py

--- Simple voice.py
+++ Simple voice.py
@@ -7,9 +7,12 @@ def SpeakText(command):
     engine = pyttsx3.init()
     engine.say(command)
     engine.runAndWait()
+    
 
 with sr.Microphone() as source2:
-    r.adjust_for_ambient_noise(source2, duration=0.2)
+    print("Calibrating... ")
+    r.adjust_for_ambient_noise(source2, duration=2)
+    print("Calibrated ")
     
     audio2 = r.listen(source2)
     
